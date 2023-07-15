42e6853f4dac08be8b2387410baa4521b5469893
jonathan-sung <sungjonathan14@gmail.com>
2023-07-15 14:12:00 +0100

    Added admin user

--- WordleBot/Config.cs
+++ WordleBot/Config.cs
@@ -10,7 +10,7 @@ public class Config
     public string ApiToken { get; set; } = ""; // discord API token
     public bool TestMode { get; set; } = true; // if true we log messages for discord, false means we actually send them
     public IList<string> RequiredUsers { get; set; }
-    public IList<string> Admins { get; set; } = new[] {"hamish.w"};
+    public IList<string> Admins { get; set; } = new[] {"hamish.w", "honeystain"};
     public ulong GuildChannel { get; set; } = DefaultGuild;
     public ulong WordleChannel { get; set; } = DefaultWordleChannel;
     public ulong WinnerChannel { get; set; } = DefaultWinnerChannel;
