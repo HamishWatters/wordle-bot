35a55f915d7fda89dc2f9f5cda710451170035dd
Hamish Watters <hamishw13@gmail.com>
2023-12-21 11:30:38 +0000

    Fix warnings

--- WordleBot/Config/Config.cs
+++ WordleBot/Config/Config.cs
@@ -9,14 +9,14 @@ public class Config
     
     public string ApiToken { get; set; } = ""; // discord API token
     public bool TestMode { get; set; } = true; // if true we log messages for discord, false means we actually send them
-    public IList<ulong> RequiredUsers { get; set; }
+    public IList<ulong> RequiredUsers { get; set; } = [];
     public IList<ulong> Admins { get; set; } = new List<ulong> {265253239295967232};
     public ulong GuildChannel { get; set; } = DefaultGuild;
     public ulong WordleChannel { get; set; } = DefaultWordleChannel;
     public ulong WinnerChannel { get; set; } = DefaultWinnerChannel;
     public ulong Bot { get; set; } = DefaultBot;
     public string ScheduledCheckTime { get; set; } = "23:59:00";
-    public IDictionary<string, IList<string>> UserNames { get; set; }
+    public Dictionary<string, IList<string>> UserNames { get; set; } = new();
     public MessageConfig Message { get; set; } = new();
     public CommandConfig Command { get; set; } = new();
 }
